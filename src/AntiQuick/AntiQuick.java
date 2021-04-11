7d37df2df813c489f7c019bcb83b3f8549dc25e4
danilcikilev <bzdanya@gmail.com>
2021-04-11 20:26:45 +0300

    AntiQuick added

---
+++ src/AntiQuick/AntiQuick.java
@@ -0,0 +1,29 @@
+package AntiQuick;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class AntiQuick {
+    public static void main(String[] args) throws IOException {
+
+        //чтение файла и создание массива длины n
+        Scanner sc = new Scanner(new File("src/AntiQuick/input.txt"));
+        int n = sc.nextInt();
+        int[] mas = IntStream.range(0, n+1).toArray();
+
+        //сам алгоритм
+        for(int i = 0; i < n; i ++) {
+            int b = mas[i];
+            mas[i] = mas[i/2];
+            mas[i/2] = b;
+        }
+
+        //запись
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(new File("src/AntiQuick/output.txt")));) {
+            writer.write(Arrays.toString(mas));
+        }
+    }
+}
